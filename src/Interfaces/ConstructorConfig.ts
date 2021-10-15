1255b52e2f375de091ef48c27057a33c413ff9aa
CodeDanielR <75735370+CodeDanielR@users.noreply.github.com>
2021-10-15 07:23:02 +0300

    Delete src directory

--- src/Interfaces/ConstructorConfig.ts
+++
@@ -1,9 +0,0 @@
-interface ConstructorConfig {
-    pluralization: boolean;
-    autoIndex: boolean;
-    useCreateIndex: boolean;
-    useNewUrlParser: boolean;
-    useUnifiedTopology: boolean;
-}
-
-export default ConstructorConfig
